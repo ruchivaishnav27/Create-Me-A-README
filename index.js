55afc75cfd08a4e03c518785c15be21e346f87a1
Ruchi Vaishnav <ruchivaishnav27@gmail.com>
2022-06-18 19:42:13 -0400

    Added code and installed Inquirer

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+inquirer
+.prompt ([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        }
+      },
+])
+.then((answers) => {})
+.catch((error) => {
+    if (error.isTtyError) {
+
+    } else {
+
+    }
+});
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +32,17 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((inquirerResponse, data) => {
+        console.log("Creating ReadMe");
+        FileSystem.writeFileSync("ReadMe.md", inquirerResponse, data);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
 
 // Function call to initialize app
-init();
+this.init();
+// or console.log
